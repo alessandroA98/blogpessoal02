9b3564bf3852bdeb3b7fab6dc4355da0f7ff7b8f
alessandro <alessandro.assuncao98@gmail.com>
2022-10-28 13:02:23 -0300

    fazendo umas alteracoes

--- src/main/java/br/org/generation/BlogPessoal/repository/PostagemRepository.java
+++ src/main/java/br/org/generation/BlogPessoal/repository/PostagemRepository.java
@@ -2,6 +2,7 @@ package br.org.generation.BlogPessoal.repository;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -11,7 +12,7 @@ import br.org.generation.BlogPessoal.model.Postagem;
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
 	
-	
+	@Autowired
 	public List<Postagem>findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
 	//trazer da Lista da Tabela, Pelo Titulo e Ignore SE é maiuscula e minuscula e Liste todos os titulos com os titulos iguais
 	
