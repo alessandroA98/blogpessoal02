131b8a5888126298d43f8ef0c0f66f4109df64e1
alessandro <alessandro.assuncao98@gmail.com>
2022-10-28 12:30:30 -0300

    dando um exclude na main

--- src/main/java/br/org/generation/BlogPessoal/BlogPessoalApplication.java
+++ src/main/java/br/org/generation/BlogPessoal/BlogPessoalApplication.java
@@ -2,8 +2,9 @@ package br.org.generation.BlogPessoal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class BlogPessoalApplication {
 
 	public static void main(String[] args) {
